45313016243d93fbbc7e4383e8e0bba943ddd994
chamath_ariyawansa <chamath.ariyawansa@workflowsapp.com>
2017-03-17 14:02:39 +0530

    configured with later js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-var agenda = module.exports = require('./lib/agenda.js');
+//var agenda = module.exports = require('./lib/agenda.js');
+
+var Agenda = require('./lib/agenda.js');
+
+var mongoConnectionString = "mongodb://wf:wf@ds119370.mlab.com:19370/jobs";
+var agenda = new Agenda({
+    db: {address: mongoConnectionString, collection: "agendaJobs"},
+    processEvery:"50 seconds",
+    maxConcurrency: 20,
+    defaultConcurrency: 5
+});
+
+
+agenda.on('ready', function () {
+    agenda.define('delete old users', function (job, done) {
+        console.log("its executing");
+    });
+});
+
+
+agenda.on('ready', function () {
+    agenda.every('0/1 * 1/1 * ? *', 'delete old users', 'datta', {timezone: 'Asia/Colombo'});
+    agenda.start();
+});
