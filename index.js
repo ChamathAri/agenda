4f01747483d8a5d1c5afde4a30dd91b2e250a664
chamath_ariyawansa <chamath.ariyawansa@workflowsapp.com>
2017-03-18 21:29:04 +0530

    fixed issues for later js intergration

--- index.js
+++ index.js
@@ -5,20 +5,26 @@ var agenda = module.exports = require('./lib/agenda.js');
 // var mongoConnectionString = "mongodb://wf:wf@ds119370.mlab.com:19370/jobs";
 // var agenda = new Agenda({
 //     db: {address: mongoConnectionString, collection: "agendaJobs"},
-//     processEvery:"50 seconds",
+//     processEvery: "50 seconds",
 //     maxConcurrency: 20,
 //     defaultConcurrency: 5
 // });
 //
+// var message = "Avenger's assemble!!!";
+//
 //
 // agenda.on('ready', function () {
-//     agenda.define('delete old users', function (job, done) {
-//         console.log("its executing");
+//     agenda.define(message, function (job) {
+//         new Promise((resolve, reject) => {
+//             console.log("Triggered -> -> :: " + job.attrs.name);
+//         }).catch((err) => {
+//             console.log("There was an error in the execution of the job" + err.stack);
+//         });
 //     });
 // });
 //
 //
 // agenda.on('ready', function () {
-//     agenda.every('0/1 * 1/1 * ? *', 'delete old users', 'datta', {timezone: 'Asia/Colombo'});
+//     agenda.every('0/1 * 1/1 * ? *', message, "Avenger's Data", {timezone: 'Asia/Colombo'});
 //     agenda.start();
 // });
